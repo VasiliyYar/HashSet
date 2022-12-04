39626727507e57dd46bb3b1a4f4f6d53789189b5
Vasiliy Solovev <vasya.79@bk.ru>
2022-12-04 21:24:20 +0300

    add List

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,17 +1,16 @@
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class WordsChecker {
     protected String text;
-    protected Set<String> set = new HashSet<>();
+    protected Set<String> set;
 
     public WordsChecker(String text) {
         this.text = text;
-        for (String s : text.split("\\P{IsAlphabetic}+")) {
-            set.add(s);
-        }
-    }
+        this.set = new HashSet<>(List.of(text.split("\\P{IsAlphabetic}+")));
 
+    }
     public boolean hasWord(String word) {
         return text.contains(word);
     }
